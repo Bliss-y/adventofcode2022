e49de41161fe888a204cd30a078c354070c558a7
nishedh <restricted.karki@gmail.com>
2023-10-21 12:43:33 +0545

    day3 part 2 (completed)

--- go/day3/day3.go
+++ go/day3/day3.go
@@ -30,31 +30,42 @@ func main() {
     }
     reader := LineReader(content);
     errorsum := 0
-    for s, i := reader(); i < len(content); s,i = reader() {
+    s, i := reader()
+    s2, _ := reader()
+    s3, _ := reader()
+    for ;i < len(content); {
 
         if(len(s) <1){
             break;
         }
-        seperator := len(s)/2
-        s1 := s[:seperator]
-        s2 := s[seperator:]
-        for _, c := range s1 {
+        for _, c := range s {
             found := false
             for i, c2 := range s2 {
                 if c == c2 {
                     s2[i] = '-'
-                    found =true 
+                    for _, c3 := range s3 {
+                        if c3 == c {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(found) {
+                        break;
+                    }
                 }
             }
             if found {
-            fmt.Printf("found %v %c \n", c, c);
             if c < 'a' {
                 errorsum += int(c - 'A') + 27
             } else {
                 errorsum +=  int(c - 'a') + 1;
             }
+            break;
+            }
         }
-        }
+    s, i = reader()
+    s2, _ = reader()
+    s3, _ = reader()
     }
     fmt.Print(errorsum)
 }
