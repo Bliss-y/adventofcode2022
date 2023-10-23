d89e9b4431dd8ecdde65e1cccde8b915abd9ada3
nishedh <restricted.karki@gmail.com>
2023-10-23 15:01:23 +0545

    day8 part2 (complete)

--- go/day8/day8.go
+++ go/day8/day8.go
@@ -10,52 +10,55 @@ func main(){
     content, _ := os.ReadFile("input.txt") 
     grid := strings.Split(string(content), "\r\n")
     length := len(grid)-1
-    visibles := make([]bool, length * length);
-    column_biggest := make([]rune, length)
+    visibles := make([]int, length * length);
     fmt.Println("LengthX, lengthY total_edge", length, len(grid[0]), length *4 -3)
     total := 0
-    position_count := 0
 
     for y,y_e := range grid {
         if(y >= length){break;}
-        curr_biggest := '/'
-        for x,x_e := range y_e {
-            position_count ++
+        for x := range y_e {
+            fmt.Println(x,y);
             position := (length) * y + x;
-        if(x >= length) {break;}
-           if(x == 0 || x_e > curr_biggest){
-               visibles[position] = true;
-               curr_biggest = x_e
-           }
-           if(y ==0 || x_e > column_biggest[x]){
-                column_biggest[x] = x_e;
-                visibles[position] = true;
-           }
-             
-        }
+            visibles[position] = getStreakX(&grid, x,y, 1) * getStreakX(&grid, x,y, -1) * getStreakY(&grid, x,y, 1) * getStreakY(&grid, x,y, -1) 
+            println("finished......", visibles[position])
+       }
     }
-    for y:=length-1; y >=0; y--{
-        y_e:= grid[y]
-        curr_biggest := '/'
-        for x:=length-1; x >=0; x--{
-            position := (length) * y + x;
-            x_e := y_e[x]
-           if(x == length-1 || rune(x_e) > curr_biggest){
-               visibles[position] = true;
-               curr_biggest = rune(x_e)
-           }
-           if(y == length-1 || rune(x_e) > column_biggest[x]){
-                column_biggest[x] = rune(x_e)
-                visibles[position] = true;
-           }
-        }
-    }
+    fmt.Println(visibles)
     for _,v := range visibles {
-        if(v){
-            total++;
+        if(v > total){
+            total = v;
         }
     }
-    fmt.Println(position_count, len(visibles))
     fmt.Println(total)
 }
 
+func getStreakY( grid *[]string, x int, y int, direction int) int{ 
+    length := len(*grid) - 1
+    if(y ==0 || y >= length -1){ return 0;}
+    streak := 0
+    for i:= y + 1*direction; i < length && i >=0; i += 1*direction {
+        fmt.Print(i);
+        streak++; 
+        if((*grid)[y][x] <= (*grid)[i][x]){
+            break;
+        }
+    } 
+    return streak
+}
+
+func getStreakX( grid *[]string, x int, y int, direction int) int{ 
+    length := len(*grid) - 1
+    if(x == 0 || x >= length-1){ return 0;}
+    streak := 0
+    for i:= x + 1*direction; i < length && i >=0 ; i += 1*direction{
+        fmt.Print(i);
+        streak++; 
+        if((*grid)[y][x] <= (*grid)[y][i]){
+            break;
+        }
+
+    } 
+    fmt.Print(" ",streak, " ");
+    println()
+    return streak
+}
